b979db68d26ee60b693edd000141305ef9225b36
MiguelMeixoeiro <m.meixoeiro@gmail.com>
2024-02-01 14:05:08 +0100

    restart

--- styles/ProjectCardStyles.jsx
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components';
-
-export const Card = styled.div`
-  background-color: ${(props) => (props.darkMode ? '#333' : '#fff')};
-  color: ${(props) => (props.darkMode ? '#fff' : '#333')};
-  // Otros estilos del componente
-`;
